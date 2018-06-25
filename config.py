1df0f31918ff4d1404d6b4111f34461f0cab08c7
elyruirui <xrf529585009@gmail.com>
2018-06-25 23:21:29 +0800

    change manage.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "hard to guess string"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
     MAIL_DEFAULT_SENDER = 'Flasky Admin <2789136271@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 465
-    MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
@@ -63,7 +63,7 @@ class ProductionConfig(Config):
 
 class HerokuConfig(ProductionConfig):
     SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
-    
+
     @classmethod
     def init_app(cls, app):
         ProductionConfig.init_app(app)
