964caa7a0e6d8f61f489efce7fd5ecc00180c8e0
elyruirui <xrf529585009@gmail.com>
2018-06-25 22:53:48 +0800

    change manage.py

--- manage.py
+++ manage.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from app import create_app, db
-from app.models import User, Role, Post, Permission, Follow, Comment
+from app.models import User, Follow, Role, Permission, Post, Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
-COV=None
-if os.environ.get('FLASK_COVERAGE'):
-    import coverage
-    COV = coverage.coverage(branch=True, include='app/*')
-    COV.start()
+
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role, Post=Post, 
-                Permission=Permission, Comment=Comment)
+    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post, Comment=Comment)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
 
 @manager.command
 def test(coverage=False):
@@ -44,7 +47,7 @@ def test(coverage=False):
 
 @manager.command
 def profile(length=25, profile_dir=None):
-    """Start the application under the code profilter."""
+    """Start the application under the code profiler."""
     from werkzeug.contrib.profiler import ProfilerMiddleware
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
                                       profile_dir=profile_dir)
@@ -53,13 +56,17 @@ def profile(length=25, profile_dir=None):
 
 @manager.command
 def deploy():
-    """Run deploment tasks."""
+    """Run deployment tasks."""
     from flask_migrate import upgrade
     from app.models import Role, User
 
+    # migrate database to latest revision
     upgrade()
 
+    # create user roles
     Role.insert_roles()
+
+    # create self-follows for all users
     User.add_self_follows()
 
 
